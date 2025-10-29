ef69975c60de7f6d244c3421f9028b26f93a4916
shedrach enike <enikeshedrack@gmail.com>
2025-10-29 11:58:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const colorBox = document.getElementById("color-box");
+  const changeColorBtn = document.getElementById("change-color-btn");
+
+  // Function to generate random HEX color
+  function getRandomColor() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+  }
+
+  // Event listener for button click
+  changeColorBtn.addEventListener("click", () => {
+    const newColor = getRandomColor();
+    colorBox.style.backgroundColor = newColor;
+  });
+});
